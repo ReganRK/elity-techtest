9d3b5bcd884f7796319bcde367c6f42bf46dab64
regan <regan@gmail.com>
2026-01-21 21:54:28 +0700

    update button when clicked

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -3,7 +3,6 @@ import Checkbox from './checkbox.tsx'
 import PrimaryButton from './primarybutton.tsx'
 
 function Container() {
-    const [count, setCount] = useState(0)
     //set how many checkbox
     const page_nunber = 6
     const pages = Array.from({ length: page_nunber }, (_, i) => i + 1);
